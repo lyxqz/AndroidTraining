1847565a0ebdfb907c4ae5baaee5e6c86bb081c0
Maoyf <lqxqzh@163.com>
2016-07-21 16:31:45 +0800

    First commit

---
+++ app/src/main/java/com/example/lsx/test_onclicklistener/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.lsx.test_onclicklistener;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "onClick: button is onClicked");
+            }
+        });
+    }
+}
